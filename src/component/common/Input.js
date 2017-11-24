a9de4d9f394baf15ac72834942f62904ce484353
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-24 10:46:12 -0800

    Create Input.js and pass Prop so that it will be able to use parent text

---
+++ src/component/common/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = ({label}) => {
+  <View>
+    <Text>({ label })</Text>
+  </View>
+};
+
+export { Input };
