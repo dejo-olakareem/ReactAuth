6b1a6c63b560bbc814f8e14192c3cc5fb25b6826
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-24 10:53:13 -0800

    Write TextInput component and manually styled it

--- src/component/common/Input.js
+++ src/component/common/Input.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({label}) => {
+const Input = ({ label }) => {
   <View>
     <Text>({ label })</Text>
+    <TextInput
+      style={{ height: 20, width: 100 }}
+    />
   </View>
 };
 
