c99c56567039658acdef1e701be868a1764fe158
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-24 08:33:46 -0800

    import LoginForm into App.js abd use Card,CardSection and Button in LoginForm.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './component/common';
+import LoginForm from './component/LoginForm';
 
 
 class App extends Component {
@@ -19,7 +20,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text> An App </Text>
+        <LoginForm />
       </View>
     );
   }
--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
-class LoginForm extends Compnent {
+class LoginForm extends Component {
   render() {
     return (
       <Card>
         <CardSection />
         <CardSection />
-        <CardSection />
+        <CardSection>
+          <Button>
+            Log in
+          </Button>
+        </CardSection>
       </Card>
     );
   }
