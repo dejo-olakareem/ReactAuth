62c689e962ddf61446e9b45c087430ab06de898b
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-24 08:22:38 -0800

    Create LoginForm file for loginForm component and import components needed from ./common 🚀

---
+++ src/component/LoginForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Compnent {
+  render() {
+    return (
+      <Card>
+        <CardSection />
+        <CardSection />
+        <CardSection />
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
