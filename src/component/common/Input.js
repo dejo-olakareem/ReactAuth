5c9b59428ccc9843eef654ca67a20bb3ee0060bd
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-24 11:03:16 -0800

    Add return to Input component to avoid error in simulator 😎

--- src/component/common/Input.js
+++ src/component/common/Input.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
 const Input = ({ label, value, onChangeText }) => {
-  <View>
-    <Text>({ label })</Text>
-    <TextInput
-      value={value}
-      onChangeText={onChangeText}
-      style={{ height: 20, width: 100 }}
-    />
-  </View>
+  return (
+    <View>
+      <Text>{ label }</Text>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={{ height: 20, width: 100 }}
+      />
+    </View>
+  );
 };
 
 export { Input };
