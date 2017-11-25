52e048c56b895b1505b03245448818d836f306b7
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-25 12:43:16 -0800

    setState for user to know if they are signed in or not using firebase.onAuthStateChanged(user).

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import LoginForm from './component/LoginForm';
 
 
 class App extends Component {
+
+  state = { loggedIn: false }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyD_hhcurzfTzDnE9N3btq_baPN7k3CT90s',
@@ -14,6 +17,14 @@ class App extends Component {
       projectId: 'authentication-5881d',
       storageBucket: 'authentication-5881d.appspot.com',
       messagingSenderId: '844572797638'
+    });
+
+    firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
   render() {
