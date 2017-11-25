29530d79388aaec22f568c273045899d0ebd3c83
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-24 16:49:27 -0800

    Write renderButton() to choose between Spinner and Button

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 
 class LoginForm extends Component {
@@ -19,6 +19,19 @@ class LoginForm extends Component {
             this.setState({ error: 'Authentication Failed' });
           });
       });
+  }
+
+
+  renderButton(){
+    if (this.state.loading) {
+      return <Spinner size="small" />
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
   }
 
   render() {
@@ -48,9 +61,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
